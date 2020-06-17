11e58d1db38056a414915ca314ddc8b037720940
jakeei212 <jake.yanov@gmail.com>
2020-06-17 14:49:11 +0300

    wow

--- server/config/keys_prod.js
+++ server/config/keys_prod.js
@@ -1,6 +1,5 @@
 module.exports = {
-    mongoURI:
-      process.env.MONGO_URI,
+    mongoURI:process.env.MONGO_URI,
     superSecret: process.env.SECRET_OR_KEY,
     cloudinaryCloudName:process.env.CLOUD_NAME,
     cloudinaryApiKey:process.env.KEY_API,
