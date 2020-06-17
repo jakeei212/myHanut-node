af59086572b2e48d2e0aeab90a9fee6b6e3a78c3
jakeei212 <53650114+jakeei212@users.noreply.github.com>
2020-06-17 15:43:13 +0300

    Delete keys_prod.js

--- server/config/keys_prod.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mongoURI:process.env.MONGO_URI,
-    superSecret: process.env.SECRET_OR_KEY,
-    cloudinaryCloudName:process.env.CLOUD_NAME,
-    cloudinaryApiKey:process.env.KEY_API,
-    cloudinaryApiSecret:process.env.SECRET_OR_API,
-  
-  };
-  
